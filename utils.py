a96cd497f6a3445fcfe9f1d9f96620f8acef94b8
xynabil <96108853+xynabil@users.noreply.github.com>
2025-12-08 13:13:02 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from constants import *
+
 def get_positive_float(prompt):
     # Validierung für positive Float-Werte
     while True:
@@ -12,14 +14,21 @@ def get_positive_float(prompt):
 
 
 def calculate_score(distance, time, pulse, training_type):
-   # Berechnet Score für Training mit Disziplin-Spezifischen Faktoren aus den Konstanten
-    from constants import SPORT_FACTORS
+    # Berechnet Score mit Disziplin-Konstanten
 
-    factors = SPORT_FACTORS.get(training_type, SPORT_FACTORS["running"]) # Sollte ein Falscher Training Type vorhanden sein wird Running genommen
+    factors = SPORT_FACTORS.get(training_type, SPORT_FACTORS["running"])
 
     pace_factor = (distance / time) * factors["pace_multiplier"]
     distance_points = distance * factors["distance_multiplier"]
     pulse_penalty = pulse / factors["pulse_divisor"]
 
-    score = distance_points + pace_factor - pulse_penalty # Berechnung des Scores
+    score = distance_points + pace_factor - pulse_penalty
     return round(score, 2)
+
+
+def get_score_category(score):
+    # Ratet den Score anhand den Konstanten
+
+    for min_score, category in SCORE_CATEGORIES:
+        if score >= min_score:
+            return category
